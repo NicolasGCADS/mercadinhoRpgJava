12dae236d587efc92abd62383794f60794a54db4
NicolasGCADS <nguinante@gmail.com>
2025-04-22 19:45:01 -0300

    Colocando Player e itens

--- src/main/java/br/com/fiap/mercadinho/model/Item.java
+++ src/main/java/br/com/fiap/mercadinho/model/Item.java
@@ -1,12 +1,80 @@
 package br.com.fiap.mercadinho.model;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "campo obrigatório")
+    @Size(min = 3, message = "deve ter pelo menos 3 caracteres")
     private String nameItem;
+
+    @NotBlank(message = "campo obrigatório")
+    @Size(min = 3, message = "deve ter pelo menos 3 caracteres")
     private String type;
+
+    @NotBlank(message = "campo obrigatório")
+    @Pattern(regexp = "^[A-Z].*", message = "As raridades são comum, raro, epico, lendario,deve começar com minuscula")
+
     private String rarity;
     private Double price;
     private String donor;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNameItem() {
+        return nameItem;
+    }
+    public void setNameItem(String nameItem) {
+        this.nameItem = nameItem;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getRarity() {
+        return rarity;
+    }
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public String getDonor() {
+        return donor;
+    }
+    public void setDonor(String donor) {
+        this.donor = donor;
+    }
+    public Item(Long id, String nameItem, String type, String rarity, Double price, String donor) {
+        this.id = id;
+        this.nameItem = nameItem;
+        this.type = type;
+        this.rarity = rarity;
+        this.price = price;
+        this.donor = donor;
+    }
+
+    
+    
     
 }
--- src/main/java/br/com/fiap/mercadinho/model/Player.java
+++ src/main/java/br/com/fiap/mercadinho/model/Player.java
@@ -1,11 +1,75 @@
 package br.com.fiap.mercadinho.model;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class Player {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "campo obrigatório")
+    @Size(min = 3, message = "deve ter pelo menos 3 caracteres")
     private String name;
+    
+    @NotBlank(message = "campo obrigatório")
+    @Pattern(regexp = "^[A-Z].*", message = "Classes devem ser guerreiro, mago ou arqueiro e para escolher digite o nome em minuscula")
     private String classe;
+
     private int level;
     private Double money;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getClasse() {
+        return classe;
+    }
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public Double getMoney() {
+        return money;
+    }
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+    public Player(Long id, String name, String classe, int level, Double money) {
+        this.id = id;
+        this.name = name;
+        this.classe = classe;
+        this.level = level;
+        this.money = money;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Player [id=" + id + ", name=" + name + ", classe=" + classe + ", level=" + level + ", money=" + money + "]";
+    }
+
+    
     
 }
